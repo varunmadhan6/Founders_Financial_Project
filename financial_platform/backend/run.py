448483fd8e513df417764d01f81f02da028e5587
varunmadhan6 <varunmadhan6@gmail.com>
2025-04-27 16:46:12 -0500

    deployment changes

--- financial_platform/backend/run.py
+++ financial_platform/backend/run.py
@@ -1,8 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
     # Use the DEBUG setting from config
-    from app.config import Config
-    app.run(debug=Config.DEBUG)
+    port = int(os.environ.get("PORT", 5000))  # default 5000 if not set
+    app.run(host="0.0.0.0", port=port)
