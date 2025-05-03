a7af2a20080b68deb27371a9ae464dcd1c856a16
amey-gupt <amey_gupta@yahoo.com>
2025-05-03 15:00:02 -0500

    mission page

--- financial_platform/frontend/src/App.jsx
+++ financial_platform/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Stock_Report_Webpage from "./webpages/Stock_Report_Webpage.jsx";
 import Login from "./webpages/Login.jsx";
 import Signup from "./webpages/Signup.jsx";
 import Profile from "./webpages/Profile.jsx";
+import Mission from "./webpages/Mission.jsx";
 import MarketPulseDashboard from "./components/MarketPulse.jsx";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
@@ -20,6 +21,7 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/mission" element={<Mission />} />
           <Route path="/market-pulse" element={<MarketPulseDashboard />} />
         </Routes>
       </>
